e39f038674f2a8d053b4edb1abfc4b01be2b7350
Artium <artium@Artiums-MacBook-Pro.local>
2020-11-30 10:41:25 +0100

    feat(workout-list/theme): Add material ui theme helpers

---
+++ apps/workout-list/src/app/utils/theme.ts
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: '#ff7f66',
+      contrastText: '#fff',
+    },
+  },
+});
