56f84ccc106556d147a85b7f5a79fa5f994ef1d8
Artium <artium@Artiums-MacBook-Pro.local>
2020-11-30 10:26:37 +0100

    feat(api/main): Add express api

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: false,
+  MONGO_DB_URL: 'mongodb://localhost:27017',
+  DB_NAME: 'gymondo',
 };
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,73 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
+import { MongoClient } from 'mongodb';
+import { environment } from './environments/environment';
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+let db;
+
+MongoClient.connect(environment.MONGO_DB_URL, (err, client) => {
+  db = client.db(environment.DB_NAME);
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/api/workouts', (request, response) => {
+  const { page, categories, month } = request.body;
+  const pageSize = 20;
+  const skip = pageSize * (page - 1);
+
+  const isCategory = categories.length !== 0;
+  const categoryFilter = isCategory ? { category: { $in: categories } } : {};
+
+  const isMonth = month !== '';
+  const monthFilter = isMonth
+    ? {
+        $where: `return this.startDate.getMonth() == ${month}`,
+      }
+    : {};
+
+  db.collection('workout')
+    .find({ ...categoryFilter, ...monthFilter }, { skip, limit: pageSize })
+    .toArray()
+    .then((docs) => response.status(200).json(docs))
+    .catch((err) => {
+      throw err;
+    });
+});
+
+app.post('/api/workout', (request, response) => {
+  const { name } = request.body;
+
+  db.collection('workout')
+    .find({ name })
+    .toArray()
+    .then((docs) => response.status(200).json(docs))
+    .catch((err) => {
+      throw err;
+    });
+});
+
+app.post('/api/count', (request, response) => {
+  const { categories, month } = request.body;
+
+  const isCategory = categories.length !== 0;
+  const categoryFilter = isCategory ? { category: { $in: categories } } : {};
+
+  const isMonth = month !== '';
+  const monthFilter = isMonth
+    ? {
+        $where: `return this.startDate.getMonth() == ${month}`,
+      }
+    : {};
+  db.collection('workout')
+    .find({ ...categoryFilter, ...monthFilter })
+    .count()
+    .then((docs) => response.status(200).json(docs))
+    .catch((err) => {
+      throw err;
+    });
 });
 
 const port = process.env.port || 3333;
