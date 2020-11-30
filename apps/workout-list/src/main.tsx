11c18a4a2a78f6abe7adb7d91c0600d3ecb51d0d
Artium <artium@Artiums-MacBook-Pro.local>
2020-11-30 10:40:35 +0100

    feat(workout-list/App): Setup the app

--- apps/workout-list/src/main.tsx
+++ apps/workout-list/src/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 import App from './app/app';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// StrictMode tag as been removed to avoid conflict with material UI
+// see : https://stackoverflow.com/questions/61220424/material-ui-drawer-finddomnode-is-deprecated-in-strictmode
+ReactDOM.render(<App />, document.getElementById('root'));
