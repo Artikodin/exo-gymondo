0cc72f98bdcdeed691bffe9d95ec1847cf1c7cb5
Artium <artium@Artiums-MacBook-Pro.local>
2020-11-30 10:42:11 +0100

    test(workout-list-e2e): Add few e2e tests

--- apps/workout-list-e2e/src/integration/app.spec.ts
+++ apps/workout-list-e2e/src/integration/app.spec.ts
@@ -1,13 +1,56 @@
-import { getGreeting } from '../support/app.po';
+import {
+  getList,
+  getListItem,
+  getTitle,
+  getButton,
+  getMonthFilter,
+  getMonthFilterList,
+  getMonthFilterListItem,
+  getTime,
+  getCategoryFilter,
+  getCategoryFilterList,
+  getCategoryFilterListItem,
+} from '../support/app.po';
 
 describe('workout-list', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to workout-list!');
+  it('should go to detail page and come back', () => {
+    getList().within(() => {
+      getListItem().eq(3).contains('workout4').click();
+    });
+    getTitle().contains('workout4');
+    getButton().click();
+    getList().within(() => {
+      getListItem().eq(3).contains('workout4');
+    });
+  });
+  it('should filter by month', () => {
+    getMonthFilter().click();
+    getMonthFilterList().within(() => {
+      getMonthFilterListItem().eq(3).contains('January').click();
+    });
+    getList().within(() => {
+      getTime()
+        .eq(2)
+        .contains(/\b(\w*January\w*)\b/);
+      getTime()
+        .eq(3)
+        .contains(/\b(\w*January\w*)\b/);
+      getTime()
+        .eq(4)
+        .contains(/\b(\w*January\w*)\b/);
+    });
+  });
+  it('should filter by category', () => {
+    getCategoryFilter().click();
+    getCategoryFilterList().within(() => {
+      getCategoryFilterListItem().eq(3).contains('c4').click();
+    });
+    getList().within(() => {
+      getListItem().eq(2).contains('c4');
+      getListItem().eq(3).contains('c4');
+      getListItem().eq(4).contains('c4');
+    });
   });
 });
--- apps/workout-list-e2e/src/support/app.po.ts
+++ apps/workout-list-e2e/src/support/app.po.ts
@@ -1,1 +1,15 @@
-export const getGreeting = () => cy.get('h1');
+export const getList = () => cy.get('ul');
+export const getListItem = () => cy.get('li');
+export const getTitle = () => cy.get('h1');
+export const getButton = () => cy.get('button');
+export const getTime = () => cy.get('time');
+
+export const getCategoryFilter = () =>
+  cy.get('[data-test-id="category-filter"]');
+export const getCategoryFilterList = () => cy.get('ul.MuiAutocomplete-listbox');
+export const getCategoryFilterListItem = () =>
+  cy.get('li.MuiAutocomplete-option');
+
+export const getMonthFilter = () => cy.get('[data-test-id="month-filter"]');
+export const getMonthFilterList = () => cy.get('ul.MuiMenu-list');
+export const getMonthFilterListItem = () => cy.get('li.MuiListItem-root');
